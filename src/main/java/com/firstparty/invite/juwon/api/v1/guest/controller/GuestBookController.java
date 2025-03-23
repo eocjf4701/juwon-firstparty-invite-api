4e99cace7ec3f84a1110712244be8cceffdedf20
parkdaechul <eocjf4701@gmail.com>
2025-03-23 16:05:05 +0900

    방명록 컨트롤러 추가

---
+++ src/main/java/com/firstparty/invite/juwon/api/v1/guest/controller/GuestBookController.java
@@ -0,0 +1,11 @@
+package com.firstparty.invite.juwon.api.v1.guest.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/guestbook")
+@CrossOrigin(origins = "*")
+public class GuestBookController {
+}
